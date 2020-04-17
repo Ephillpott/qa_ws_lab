fd52599fee0168f2eca6ad995d63f4ef20ab3a7f
brian parra <brianparra@brians-iMac.local>
2020-04-16 17:09:26 -0700

    Classwork

--- qa/src/main/java/Main.java
+++ qa/src/main/java/Main.java
@@ -22,8 +22,41 @@ public class Main {
     Assert.assertEquals(isEven(8), true);
   }
 
+  @Test
+  public void testExtractGetParamValueFound(){
+    Assert.assertEquals(extractGetParam("http://localhost:1234?abc=123", "abc"),
+            "123");
+  }
+
+  @Test
+  public void testExtractGetParamMissingKey(){
+    // happens to work
+    Assert.assertEquals(extractGetParam("http://localhost:1234?abcd=123", "abc"),
+            null);
+  }
+
+  @Test
+  public void testMultipleParams(){
+    // fails because it doesn't support multiple params
+    Assert.assertEquals(extractGetParam("http://localhost:1234?abc=123&b=4", "abc"),
+            "123");
+  }
+
+  // Business logic
   public static boolean isEven(int j){
-    return false;
+    return j % 2 == 0; // clearly wrong
+  }
+
+  public static String extractGetParam(String url, String key){
+    String[] parts = url.split(":");
+    System.out.println(parts[2]);
+    String[] portParts = parts[2].split("\\?");
+    System.out.println(portParts[1]);
+    String[] keyValueParts = portParts[1].split("=");
+    if(keyValueParts[0].equals(key)){
+      return keyValueParts[1];
+    }
+    return null; // doesn't work
   }
 
 }
--- ws_server/src/main/java/demo/SparkDemo.java
+++ ws_server/src/main/java/demo/SparkDemo.java
@@ -5,7 +5,8 @@ import static spark.Spark.*;
 public class SparkDemo {
   public static void main(String[] args) {
     port(1235);
-    webSocket("/ws", WebSocketHandler.class);
-    get("/hello", (req, res) -> "hi");
+    // 2 way communication
+    webSocket("/ws", WebSocketHandler.class); // open socket and leave it open
+    get("/hello", (req, res) -> "hi"); // test
   }
 }
